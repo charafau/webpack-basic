2bec33e3224791cca66b47916504fb655b74fee2
rafal.wachol <rafal.wachol@ocado.com>
2017-11-01 16:40:23 +0000

    added babel cli

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,19 +1,23 @@
+'use strict';
+
 function component() {
     var element = document.createElement('div');
 
-    let test = ['dog', 'doge', 'doggy'];
+    var test = ['dog', 'doge', 'doggy'];
 
-    let text = test.map(animal => animal + '1')
+    var text = test.map(function (animal) {
+        return animal + '1';
+    });
 
-    let wholeText = text.reduce((sum, animal) => sum + ' ' + animal, '');
+    var wholeText = text.reduce(function (sum, animal) {
+        return sum + ' ' + animal;
+    }, '');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'yeti', wholeText], ' ');
 
-
     return element;
-    
 }
 
-
 document.body.appendChild(component());
+//# sourceMappingURL=index.js.map
